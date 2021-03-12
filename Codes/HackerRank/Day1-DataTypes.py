0ad3e5f2d9cbf2dd2d197fe5cae0c98b3c232bf4
bsudharshan2001 <59043812+bsudharshan2001@users.noreply.github.com>
2021-03-12 11:23:54 +0530

    Create Day1-DataTypes.py

---
+++ Codes/HackerRank/Day1-DataTypes.py
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables
+m=4.0
+# Read and save an integer, double, and String to your variables.
+j=int(input())
+n=float(input())
+s=input()
+# Print the sum of both integer variables on a new line.
+print(i+j)
+# Print the sum of the double variables on a new line.
+print(m+n)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print("HackerRank "+s)
